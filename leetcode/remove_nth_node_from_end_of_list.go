e58d3a5fc710f41d45418c2606abbaac026c9f25
liuxu <928525166@qq.com>
2019-11-29 10:25:16 +0800

    解决删除倒数n个节点，在链表上的算法

--- leetcode/remove_nth_node_from_end_of_list.go
+++ leetcode/remove_nth_node_from_end_of_list.go
@@ -21,7 +21,9 @@ import (
 //Follow up:
 //
 //Could you do this in one pass?
-
+//基本处理链表的的节点问题，如果是正顺序，那么很好处理，就是直接遍历就行
+//但是如果是倒序处理的，那就要至少两个指针了，或者是，你可以首先翻转链表，转化为正序
+//之后再次翻转链表就行，只是o(n)多个了常数倍而已
 type UserNode struct {
 	userName string
 	age      int
@@ -30,14 +32,20 @@ type UserNode struct {
 
 func RemoteNthNodeFromEndOfList(sequence int) {
 	head := constructList(10)
+	doRemove(head, 3)
 	fmt.Print(head)
 }
 
 func doRemove(head *UserNode, sequence int) *UserNode {
 	first, second := head, head
 	for index := 0; index < sequence; index++ {
-
+		first = first.next
 	}
+	for first.next != nil {
+		second = second.next
+		first = first.next
+	}
+	second.next = second.next.next
 	return head
 }
 
