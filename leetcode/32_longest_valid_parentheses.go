6b1b2b3ff2186f023208a9e26e133a43d46e672e
liuxu <928525166@qq.com>
2020-08-27 18:45:42 +0800

    真的好吵啊，不知道整天搞什么剑不剑的，真的是

---
+++ leetcode/32_longest_valid_parentheses.go
@@ -0,0 +1,40 @@
+package leetcode
+
+import "fmt"
+
+//32. Longest Valid Parentheses
+//Hard
+//
+//3712
+//
+//139
+//
+//Add to List
+//
+//Share
+//Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+//
+//Example 1:
+//
+//Input: "(()"
+//Output: 2
+//Explanation: The longest valid parentheses substring is "()"
+//Example 2:
+//
+//Input: ")()())"
+//Output: 4
+//Explanation: The longest valid parentheses substring is "()()"
+//
+// 这道题目看着才是正常的题目，之前的很多题目感觉要写很多的判断和小代码，没有那种一个方案就能解决
+// 的方式，例如递归了，动态规划啥的
+//
+
+func LongestValidParentheses() {
+	source := ")()()("
+	result := doLetterCombinationsOfAPhoneNumber(source)
+	fmt.Printf("结果是:%d\n", result)
+}
+
+func doLongestValidParentheses(source string) int {
+
+}
