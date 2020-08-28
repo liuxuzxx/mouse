cdffbe050fe14dc285a230493012615b050507ec
liuxu <928525166@qq.com>
2020-08-28 19:15:56 +0800

    使用下标来计算最长

--- leetcode/32_longest_valid_parentheses.go
+++ leetcode/32_longest_valid_parentheses.go
@@ -1,6 +1,9 @@
 package leetcode
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 //32. Longest Valid Parentheses
 //Hard
@@ -27,14 +30,36 @@ import "fmt"
 //
 // 这道题目看着才是正常的题目，之前的很多题目感觉要写很多的判断和小代码，没有那种一个方案就能解决
 // 的方式，例如递归了，动态规划啥的
-//
+// ( 40 ) 41
 
 func LongestValidParentheses() {
-	source := ")()()("
-	result := doLetterCombinationsOfAPhoneNumber(source)
+	source := "()(()"
+	result := doLongestValidParentheses(source)
 	fmt.Printf("结果是:%d\n", result)
 }
 
 func doLongestValidParentheses(source string) int {
+	stack := ParenthesesStack{
+		elements:     make([]int, 0),
+		currentIndex: 0,
+	}
 
+}
+
+type ParenthesesStack struct {
+	elements     []int
+	currentIndex int
+}
+
+func (p *ParenthesesStack) push(element int) {
+	p.elements = append(p.elements, element)
+	p.currentIndex = p.currentIndex + 1
+}
+
+func (p *ParenthesesStack) pop() (int, error) {
+	if p.currentIndex > 0 {
+		p.currentIndex = p.currentIndex - 1
+		return p.elements[p.currentIndex], nil
+	}
+	return -1, errors.New("越界了")
 }
